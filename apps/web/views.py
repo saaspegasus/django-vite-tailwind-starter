22ac7294fca80c3ab4eb33027c9899275d31add8
Cory Zue <cory@coryzue.com>
2024-11-12 14:05:04 +0200

    basic app layout

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,6 +1,10 @@
 from django.template.response import TemplateResponse
-
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return TemplateResponse(request, 'web/landing_page.html')
 
+
+@login_required
+def app_home(request):
+    return TemplateResponse(request, 'web/app_home.html')

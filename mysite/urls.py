3360a3dc340477e01b80b031980f59df4d06bd0d
Cory Zue <cory@coryzue.com>
2025-10-29 16:31:04 +0200

    rename file

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html")),
+    path('hello-vite/', TemplateView.as_view(template_name='hello_vite.html'))
 ]

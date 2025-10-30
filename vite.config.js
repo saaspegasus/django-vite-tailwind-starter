6f657727bb459b5ec2e99dea6bd56108a99475e9
Cory Zue <cory@coryzue.com>
2025-10-30 16:05:58 +0200

    improve config comments

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './assets/javascript'),
     },
   },
-  base: '/static/', // Should match Django's STATIC_URL
+  base: '/static/', // This should match Django's STATIC_URL
   build: {
-    outDir: path.resolve(__dirname, './static'), // Output directory for production build
+    outDir: path.resolve(__dirname, './static'), // This should be something in your STATICFILES_DIRS
     emptyOutDir: false, // Preserve the outDir to not clobber Django's other files.
     manifest: "manifest.json",
     rollupOptions: {
